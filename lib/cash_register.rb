ce426191866b0fb1720a9e7f817838c320bec1bd
Erin  Rose <errose10@gmail.com>
2020-01-08 15:55:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
 
 attr_accessor :cash_register, :cash_register_with_discount
 
-  def initialize(discount = nil)
+  def initialize(employee_discount = nil)
     @total = 0
   end
 end
